919fb6d70e0cdb7915268c6426a62dbfcfe9ad79
nkietwee <nawaporn.kiet@gmail.com>
2023-04-11 23:28:58 +0700

    rewrite

---
+++ real/ft_fdf.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fdf.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkietwee <nkietwee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/11 14:39:46 by nkietwee          #+#    #+#             */
+/*   Updated: 2023/04/11 16:16:41 by nkietwee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	main(int ac, char **av)
+{
+	// t_keyhook display;
+	t_fillnbr nbr;
+
+	// display.mlx = mlx_init(); //connection to the correct graphical system
+	// display.mlx_win = mlx_new_window(display.mlx, 1920, 1080, "Hello world!"); // create window
+	// display.img.img = mlx_new_image(display.mlx, 1920, 1080); //create
+	// display.img.addr = mlx_get_data_addr(display.img.img, &display.img.bits_per_pixel, &display.img.line_length,
+	// 							&display.img.endian); // put 0,0 first point
+
+	ft_checkmap(ac);
+	ft_count(&nbr, av[1]);
+
+
+
+	// mlx_put_image_to_window(display.mlx, display.mlx_win, display.img.img, 0, 0); // put pic to window
+	// mlx_loop(display.mlx); // loop window
+
+}
---
+++ tmp/libft/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkietwee <nkietwee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 19:04:24 by nkietwee          #+#    #+#             */
+/*   Updated: 2022/10/13 13:43:53 by nkietwee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	symbol;	
+	int	result;
+
+	i = 0;
+	symbol = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-')
+	{
+		i++;
+		symbol = -1;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 +(str[i] - '0');
+		i++;
+	}	
+	return (result * symbol);
+}
